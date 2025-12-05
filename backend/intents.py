7b3879720bdfe5be353b89f5598a600750d24b99
tricar534 <tricar534@gmail.com>
2025-12-05 00:13:42 -0800

    Create intents.py
    Start of intent rules and logic

---
+++ backend/intents.py
@@ -0,0 +1,9 @@
+import re
+
+# labels for intent
+INTENT_GREETING = "greeting"
+INTENT_INGREDIENT = "ingredient_search"
+INTENT_MEAL_PLAN = "meal_plan"
+INTENT_DIET = "diet_restrictions"
+INTENT_OTHER = "unknown_intent"
+
