093db68eea98007832e0a6e1c07714c651ef8a3b
tricar534 <tricar534@gmail.com>
2025-12-05 00:51:10 -0800

    Finish simple regex patterns

--- backend/intents.py
+++ backend/intents.py
@@ -1,9 +1,24 @@
 import re
 
-# labels for intent
+# Intent labels
 INTENT_GREETING = "greeting"
 INTENT_INGREDIENT = "ingredient_search"
 INTENT_MEAL_PLAN = "meal_plan"
 INTENT_DIET = "diet_restrictions"
 INTENT_OTHER = "unknown_intent"
+
+# Simple regex patterns
+
+GREETING_PATTERN = re.compile(r"\b(hi|hello|hey)\b", re.IGNORECASE)
+
+INGREDIENT_PATTERN = re.compile(r"\bi\s*have\s+(.+)", re.IGNORECASE)
+
+MEAL_PLAN_PATTERN = re.compile(r"\b(meal plan|plan my meal|plan my meals|make a meal plan)\b", re.IGNORECASE)
+
+DIET_PATTERN = re.compile(r"\b(vegetarian|vegan|high protein|low carb|keto)\b", re.IGNORECASE)
+
+DIET_KEYWORDS = ["vegetarian", "vegan", "high protein", "low carb", "keto"]
+
+
+
 
